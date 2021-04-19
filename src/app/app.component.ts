14cbf5b0dad3257c71ffd4102e2adc955f2de334
Suryateja <suryateja.gujju@gmail.com>
2021-04-19 23:23:13 +0530

    todoapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'ng2todo';
+
+  todos=[
+    {name: 'first', done: false},
+    {name: 'second', done: true},
+    {name: 'third', done: false}
+  ];
+
+  newTodo="";
+
+  remain = ()=> {
+    let c = 0;
+    this.todos.forEach(t => {
+      if(!t.done) {
+        c++;
+      }
+      
+    });
+    return c;
+  };
+
+
+
+archive() {
+  const oldTodos =this.todos;
+  this.todos=[];
+  oldTodos.forEach(t =>{
+
+  if(!t.done) {
+    this.todos.push(t);
+  }
+});
+};
+add() {
+  let t = { name: this.newTodo, done:false};
+  this.todos.push(t);
+  this.newTodo='';
+};
 }
